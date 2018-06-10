c09ce5cfea5ceaa96f8e3ead202fe3c29d0b0185
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-10 15:44:47 -0400

    On the single page application allow different components to render based on routes.

--- src/app/RxjsExample.js
+++ src/app/RxjsExample.js
@@ -6,12 +6,24 @@ import { LandingPage } from './landingPage/LandingPage.js';
 // import {TestSuiteIntroduction} from './testSuiteIntroduction/TestSuiteIntroduction.js'
 // import {TestSuiteIntroduction} from './testSuiteIntroduction/OldTestSuiteIntroduction.js'
 
+function determineComponents(currRoute) {
+  {
+    console.log(currRoute)
+    switch (currRoute) {
+      case '/': return (<LandingPage rxpng={rxpng}/>); break;
+      // case '/main_landing_page.html': return (<TestSuiteIntroduction rxpng={rxpng}/>); break;
+      // case '/main_rxjs_page.html': return (<RxjsRunner/>); break;
+      default: return (<LandingPage rxpng={rxpng}/>);
+    }
+  }
+}
+
 class App extends Component {
   render() {
   let currRoute = window.location.href.substring(21)
     return (
       <div>
-        <LandingPage rxpng={rxpng}/>
+        {determineComponents(currRoute)}
       </div>
     );
   }
