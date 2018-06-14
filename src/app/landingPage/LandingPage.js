43ca6507b0ea22a423d09413013aecff5857c59f
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-14 07:21:29 -0400

    load image as a centered background element.

--- src/app/landingPage/LandingPage.js
+++ src/app/landingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const  EntryMessage = () => {
   return (
-    <div className="reposition">
+    <div>
       <h1>RxJS the way of the future.</h1>
       <p>examples in slim supply.</p>
       <a href="main_landing_page.html">click here</a>
@@ -12,8 +12,9 @@ const  EntryMessage = () => {
 export const LandingPage = ({namedContent, additionalContent, rxpng}) => {
   return (
     <div>
-      <img src={rxpng} id="loading" alt="" />
+      <img src={rxpng} id="loading" alt="" className="reposition-image"/>
       <EntryMessage/>
+      <br/><br/><br/><br/>Important to know, use npm as go to website for rxjs: https://www.npmjs.com/package/rxjs
     </div>
   )
 }
