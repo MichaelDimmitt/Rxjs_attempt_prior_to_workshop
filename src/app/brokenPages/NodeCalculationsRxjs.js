f57bd234ceec63ae83fc9aace904bf28faf9919e
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-14 15:15:11 -0400

    gather even numbers from 1 to 400. in cosole.

--- src/app/brokenPages/NodeCalculationsRxjs.js
+++ src/app/brokenPages/NodeCalculationsRxjs.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../../assets/css/additionalStyling.css';
+import 'rxjs/add/observable/of';
+  import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+  import { map, filter, switchMap } from 'rxjs/operators';
 // const  EntryMessage = () => {
 //   return (
 //    <div className="reposition">
@@ -7,12 +10,20 @@ import '../../assets/css/additionalStyling.css';
 //    </div>
 //   )
 // }
+function map_explination_marks(){
+
+  range(1, 200)
+    .pipe(filter(x => x % 2 === 1), map(x => x + x))
+    .subscribe(x => console.log(x));
+  return <div>Observable.of(1,2,3).map(x => x + '!!!')</div>
+}
 
 export const NodeCalculationsRxjs = ({rxpng}) => {
   return (
     <div>
       future efforts will go into this implementation
-
+      {map_explination_marks()}
+      <a href="/brokenPages/game.html">click here</a>
     </div>
   )
 }
